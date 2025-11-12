00132a883a9bad38249e5422fc000e71151715fd
Bayron Alpízar Quesada <bayalpiizar777@gmail.com>
2025-11-12 10:05:42 -0600

    URL mapping change to /

--- src/main/java/com/roomiefy/properties_service/application/rest/PropertyController.java
+++ src/main/java/com/roomiefy/properties_service/application/rest/PropertyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/properties")
+@RequestMapping("/")
 public class PropertyController {
 
     private final PropertyServicePort propertyServicePort;
