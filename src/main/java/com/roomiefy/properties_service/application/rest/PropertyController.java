4ea151a09a09cdf009a5416df57c2b95ac96bc29
Bayron Alpízar Quesada <bayalpiizar777@gmail.com>
2025-11-15 00:20:52 -0600

    docs: update comment for base path in PropertyController

--- src/main/java/com/roomiefy/properties_service/application/rest/PropertyController.java
+++ src/main/java/com/roomiefy/properties_service/application/rest/PropertyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/") // Base path for property-related endpoints
 public class PropertyController {
 
     private final PropertyServicePort propertyServicePort;
